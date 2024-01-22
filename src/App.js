b9f2a987a88529e0319296a07b579dc92608f720
santosh-rawat-prof <santoshrawat.prof@gmail.com>
2024-01-22 19:02:25 +0530

    Added File Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <h1>welcome to the agriculture</h1>
-  );
+  return <h1>welcome to the agriculture</h1>;
 }
 
 export default App;

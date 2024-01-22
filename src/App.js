ecdfc28042c9ba5b84e4f777e6e6abc46bdc55b4
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2024-01-23 00:22:04 +0530

    auth-check

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-
-import './App.css';
-import { auth } from './firebase/config';
+import "./App.css";
+import { auth } from "./firebase/config";
 function App() {
-  console.log(auth)
+  console.log(auth);
   return (
     <div>
-    <h1>welcome to the agriculture</h1>
+      <h1>welcome to the agriculture</h1>
     </div>
   );
-import "./App.css";
-
-function App() {
-  return <h1>welcome to the agriculture</h1>;
 }
-
-export default App;

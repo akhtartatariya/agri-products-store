50699450b3505e708177a3a242fb11f7db2ab347
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2024-01-27 01:56:26 +0530

    add getFirestore and create database

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { conf } from "../conf/conf";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,5 +11,5 @@ import {getAuth} from 'firebase/auth'
 
 // Initialize Firebase
 export const app = initializeApp(conf);
-export const auth=getAuth(app);
-
+export const fireDB = getFirestore(app);
+export const auth = getAuth(app);

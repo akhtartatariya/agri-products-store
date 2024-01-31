f9967be616696e0414500d373e53ea9028eddb40
anshika sharma <anshikasharma.as19@gmail.com>
2024-01-31 20:39:15 +0530

    added banner to about

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+
 function About() {
   return (
     <>
       <div className="h-14 w-full text-white text-sm pl-[3%] md:pl-[7%] bg-[#0073cf] flex flex-row items-center">
         <Link to={"/"}>Home</Link> &nbsp;/ About us
       </div>
+      <div>
+       <img src="./img/about/aboutbanner.jpg" alt="about banner" className="h-72 w-full object-cover object-bottom"/>
+      </div>
     </>
   );
 }
 
 export default About;
+
+

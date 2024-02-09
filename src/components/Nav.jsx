f2c7861365c5f98d45865d856157060cf34f6fc7
santosh-rawat-prof <santoshrawat.prof@gmail.com>
2024-02-09 23:53:56 +0530

    Fixed a UI bug in nav for tab view

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -198,8 +198,10 @@ function Nav() {
             )}
           </ul>
         </div>
-        <div className="w-[10rem] flex text-2xl text-[#0073cf] justify-end gap-3 md:gap-8 z-30">
-          <Link to={`/user-profile/${userName}`}><BiSolidUser className="cursor-pointer" /></Link>
+        <div className="w-[12rem] flex text-2xl text-[#0073cf] justify-end gap-3 md:gap-8 z-30">
+          <Link to={`/user-profile/${userName}`}>
+            <BiSolidUser className="cursor-pointer" />
+          </Link>
           {userEmail !== "a@gmail.com" && userEmail !== "sanaya@gmail.com" ? (
             <div className="relative flex">
               <MdOutlineShoppingCart

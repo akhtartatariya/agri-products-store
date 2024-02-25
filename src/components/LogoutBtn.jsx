a3be8e735ed2b4146ea36bb35add8818810769c6
santosh-rawat-prof <santoshrawat.prof@gmail.com>
2024-02-25 12:18:22 +0530

    Debugged Logout function which was redirecting to not existing path

--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -7,17 +7,21 @@ import { useNavigate } from "react-router-dom";
 const LogoutBtn = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleCllck = () => {
-    authService.logout().then(() => {
+  const handleClick = async () => {
+    try {
+      await authService.logout(); // Wait for logout to complete
       dispatch(logout());
       localStorage.clear("user");
       navigate("/login/user");
-    });
+    } catch (error) {
+      // Handle logout error
+      console.error("Error during logout:", error);
+    }
   };
   return (
     <button
       className="px-3 py-2 bg-[#0073cf] text-white rounded hover:bg-sky-500"
-      onClick={handleCllck}
+      onClick={handleClick}
     >
       Logout
     </button>

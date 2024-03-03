d008b9540ecab79f3515cecb3d7a2b086e466104
santosh-rawat-prof <santoshrawat.prof@gmail.com>
2024-03-03 17:54:34 +0530

    Netlify form handling

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -150,7 +150,7 @@ function Contact() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-6 m-4 md:m-20">
           <div className="col-span-1 md:col-span-4">
-            <form className="" onSubmit={handleSubmit}>
+            <form className="" onSubmit={handleSubmit} netlify>
               <div className="mb-8">
                 <label
                   htmlFor="name"
